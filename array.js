bf3426a6c0aefcd9348449515e128bdd2552440f
Ashish Nishad <ashishnishad@Ashishs-MacBook-Air.local>
2025-01-20 00:21:07 +0530

    Added initial DSA code

--- array.js
+++ array.js
@@ -9,9 +9,7 @@ const arr = [10, 20, 30, 40, 50];
 // 	console.log(`the value of the ${value} and index of the element ${index}`)
 // })
 
-// console.log(arr.length - 1)
 
 for(i=0; i<arr.length; i++){
 	console.log(`index of the array ${i} and value of the array ${arr[i]}` )
 }
-
