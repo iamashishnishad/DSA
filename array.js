d94d00b5157e612d9b272f8d6f0ab5c8f7a67fec
Ashish Nishad <ashishnishad@Ashishs-MacBook-Air.local>
2025-01-20 00:17:04 +0530

    Initial commit

---
+++ array.js
@@ -0,0 +1,17 @@
+///Traverse the array and print the sum of all the elements in the array
+
+const arr = [10, 20, 30, 40, 50];
+
+
+/// Using the forEach
+
+// arr.forEach((value, index) => {
+// 	console.log(`the value of the ${value} and index of the element ${index}`)
+// })
+
+// console.log(arr.length - 1)
+
+for(i=0; i<arr.length; i++){
+	console.log(`index of the array ${i} and value of the array ${arr[i]}` )
+}
+
